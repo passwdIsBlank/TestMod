f35331d1f42aff22637f01a888a7f0198e25c67a
passwdIsBlank <ajavierirazu@gmail.com>
2024-06-28 23:03:51 +0200

    Modified template

---
+++ TestMod/TestMod.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace TestMod
+{
+	public class TestMod : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(TestMod)} has loaded!");
+		}
+	}
+}
